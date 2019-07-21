5a5d5772114352d0efed5863b8001d12e7909499
pranavlarry <pranavlarry111@gmail.com>
2019-07-21 17:45:53 +0530

    5 components

--- src/assets/scripts/demo.js
+++ src/assets/scripts/demo.js
@@ -1,28 +1,28 @@
-console.log('Demo!');
+// console.log('Demo!');
 
-$(".menu").click(function(){
-  $(".nav").toggleClass("open");
-});
+// $(".btn").click(function(){
+//   $(".btn").toggleClass("minu");
+// });
 
 $(window).scroll(function(e){
     parallax();
   });
 
-const $navbar = $(".nav"),y_pos = $navbar.offset().top,height = $navbar.height();
+// const $navbar = $(".nav"),y_pos = $navbar.offset().top,height = $navbar.height();
 
-//scroll top 0 sticky
-$(document).scroll(function() {
-  let scrollTop = $(this).scrollTop();
-  if (scrollTop > 0) {
-    $navbar.addClass("sticky");
-  } else {
-    $navbar.removeClass("sticky");
-  }
-});
+// //scroll top 0 sticky
+// $(document).scroll(function() {
+//   let scrollTop = $(this).scrollTop();
+//   if (scrollTop > 0) {
+//     $navbar.addClass("sticky");
+//   } else {
+//     $navbar.removeClass("sticky");
+//   }
+// });
 
 function parallax(){
     var scrolled = $(window).scrollTop();
-    $('.hero').css('top',-(scrolled*0.0315)+'rem');
-    $('.hero > h1').css('top',-(scrolled*-0.005)+'rem');
-    $('.hero > h1').css('opacity',1-(scrolled*.00175));
+    $('.jumbotron__container').css('top',-(scrolled*0.0315)+'rem');
+    // $('.hero > .hero__text > h1').css('top',-(scrolled*-0.005)+'rem');
+    // $('.hero > .hero__text > h1').css('opacity',1-(scrolled*.00175));
   };
